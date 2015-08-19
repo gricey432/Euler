7753e5ef8372fb035859d45f5360679427ed7d4c
Mitchell Grice <mitch@gricey.net>
2015-08-19 16:43:41 +1000

    Finished 5

---
+++ 5.py
@@ -0,0 +1,7 @@
+n = 20
+while True:
+    if not sum([n % d for d in [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]):
+        break
+    n += 20
+
+print n
---
+++ 5_nonpythonic.py
@@ -0,0 +1,15 @@
+n = 20
+while True:
+    if  not n % 11\
+    and not n % 12\
+    and not n % 13\
+    and not n % 14\
+    and not n % 15\
+    and not n % 16\
+    and not n % 17\
+    and not n % 18\
+    and not n % 19\
+    and not n % 20:
+        break;
+    n += 20
+print n
