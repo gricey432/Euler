b39937d68c2ecc644c6d21071b5abb47148fc668
Mitchell Grice <mitch@gricey.net>
2015-08-20 17:15:05 +1000

    Added caching to 14

--- 14.py
+++ 14.py
@@ -1,13 +1,15 @@
-fc = 0
+collatz_cache = {}
 
 def collatz_counter(n):
-    global fc
-    fc += 1
-    if n == 1:
-        return 1
-    if n%2 == 0:
-        return collatz_counter(n / 2) + 1
-    return collatz_counter(3 * n + 1) + 1
+    global collatz_cache
+    if n not in collatz_cache:
+        if n == 1:
+            collatz_cache[n] = 1
+        elif n%2 == 0:
+            collatz_cache[n] = collatz_counter(n / 2) + 1
+        else:
+            collatz_cache[n] = collatz_counter(3 * n + 1) + 1
+    return collatz_cache[n]
 
 longest_num = 0
 longest_length = 0
