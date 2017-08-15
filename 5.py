a1f705516777f068e794426f07d2462ac5b1f56a
Mitchell Grice <mitch@gricey.net>
2017-08-15 13:38:09 +1000

    Fixed old broken ones

--- 5.py
+++ 5.py
@@ -1,6 +1,6 @@
 n = 20
 while True:
-    if not sum([n % d for d in [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]):
+    if not sum([n % d for d in [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]]):
         break
     n += 20
 
